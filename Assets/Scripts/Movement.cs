c16144ab3dc9703788aaf730776faea4d8dde404
LeonVilagos <leonv.profi@gmail.com>
2026-02-28 18:30:49 +0100

    Sretno :)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,24 +1,25 @@
-using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.HID;
-using UnityEngine.Rendering;
+
 
 public class Movement : MonoBehaviour
 {
     private float moveVertical, moveHorizontal;
-    private float rotateVertical, rotateHorizontal; 
-    [SerializeField]
-    private float speed = 4.0f, sprintSpeed; 
+    private float rotateVertical, rotateHorizontal;
+    private float sprintSpeed;
+    
     private Vector3 movementVector;
+
+    Rigidbody rb;
+
+    [SerializeField]
+    private float jumpForce = 5.0f;
+    [SerializeField]
+    private float speed = 4.0f;
     [SerializeField]
     private GameObject CameraObject;
     [SerializeField]
     InputAction jump;
-    [SerializeField]
-    float jumpForce = 5.0f;
-
-    Rigidbody rb;
 
     private void Start()
     {
@@ -27,7 +28,11 @@ public class Movement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        jump.Enable();
+    }
+
     void Update()
     {
         HandleMovement();
@@ -36,19 +41,7 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        RaycastHit hit;
-        if (jump.IsPressed())
-        {
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.01f))
-            {
-                rb.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
-            }
-        }
-    }
-
-    private void OnEnable()
-    {
-        jump.Enable();
+        HandleJumping();
     }
 
     void HandleMovement()
@@ -69,17 +62,25 @@ public class Movement : MonoBehaviour
 
         transform.Translate(movementVector.normalized * speed * sprintSpeed * Time.deltaTime);
     }
-
     void HandleRotation()
     {
         rotateHorizontal += Input.GetAxis("Mouse X");
         rotateVertical += Input.GetAxis("Mouse Y");
 
-        //Debug.Log(rotateVertical + " " + rotateHorizontal);
         rotateVertical = Mathf.Clamp(rotateVertical, -90, 90);
 
         transform.rotation = Quaternion.Euler(0f, rotateHorizontal, 0f);
         CameraObject.transform.rotation = Quaternion.Euler(-1*rotateVertical, rotateHorizontal, 0f);
-        
+    }
+    void HandleJumping()
+    {
+        RaycastHit hit;
+        if (jump.IsPressed())
+        {
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.01f))
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
     }
 }
